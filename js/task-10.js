28ba1f02ae5e63130055c98e287c0f178d30f031
Igorippon <botvunaigor@gmail.com>
2023-06-20 20:05:59 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,62 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+ input: document.querySelector('input'),
+ createBtn: document.querySelector('[data-create]'),
+ destroyBtn: document.querySelector('[data-destroy]'),
+ divEl: document.querySelector('#boxes')
+}
+
+refs.input.addEventListener('blur', handlerInput);
+
+function handlerInput(evt) {   
+  const amount = evt.currentTarget.value;
+
+  refs.createBtn.addEventListener('click', handlerClick);
+
+  function handlerClick() {
+    createBoxes(amount);
+  };
+  
+  refs.destroyBtn.addEventListener('click', () => {
+    destroyBoxes();
+    refs.createBtn.removeEventListener('click', handlerClick);
+  });
+  
+}
+
+function createBoxes(amount) {
+  let width = 30;
+  let height = 30;
+  const divs = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement("div");
+    width += 10;
+    height += 10;
+    div.style.width = `${width}px`;
+    div.style.height = `${height}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    divs.push(div);
+  };
+
+  refs.divEl.append(...divs);
+}
+  
+function destroyBoxes() {
+  refs.divEl.innerHTML = '';
+      refs.input.value = '';
+}
+
+
+
+
+
+
+
+
+
+ 
+
